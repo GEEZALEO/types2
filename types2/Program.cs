07a7c781ef8fbf040868b88ad3988e5470063c19
GEEZALEO <44001390+GEEZALEO@users.noreply.github.com>
2020-02-18 10:05:36 -0500

    finished chap 5
    error on 7 and 13

---
+++ types2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "George";
+            int grade = 98;
+            char lettergrade = 'A';
+            double costofTuition = 15.00;
+
+
+            Console.WriteLine($"{firstname} is the student, {grade} is the grade earned. which is an {lettergrade} and it cost {costofTuition}");
+            Console.ReadLine();
+
+        }
+    }
+}
+
